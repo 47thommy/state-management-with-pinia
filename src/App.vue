d50bb744bdab07b006f64204f91dd3fa64f2b729
47thommy <47thommy@gmail.com>
2024-10-11 23:57:05 +0300

    feat: implement getters in the state

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,19 @@
 			<img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo" />
 			<h1>Pinia Tasks</h1>
 		</header>
-		<div class="task-list">
+		<nav class="filter">
+			<button @click="filter = 'all'">All tasks</button>
+			<button @click="filter = 'favs'">Fav tasks</button>
+		</nav>
+		<div v-if="filter === 'all'" class="task-list">
+			<p>You have {{ taskStore.totalCount }} tasks left to do.</p>
 			<div v-for="task in taskStore.tasks" :key="task.id">
+				<TaskDetail :task="task" />
+			</div>
+		</div>
+		<div v-if="filter === 'favs'" class="task-list">
+			<p>You have {{ taskStore.favCount }} tasks in your favs list.</p>
+			<div v-for="task in taskStore.favs" :key="task.id">
 				<TaskDetail :task="task" />
 			</div>
 		</div>
@@ -16,5 +27,7 @@
 <script setup>
 	import { useTaskStore } from "./stores/TaskStore";
 	import TaskDetail from "./components/TaskDetail.vue";
+	import { ref } from "vue";
 	const taskStore = useTaskStore();
+	const filter = ref("all");
 </script>
