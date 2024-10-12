77ebeae9ea86a57f713c89f22b8ce85f10a9425a
47thommy <47thommy@gmail.com>
2024-10-12 13:56:48 +0300

    feat: Resetting State and using storeToRefs

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,16 @@
 			<button @click="filter = 'all'">All tasks</button>
 			<button @click="filter = 'favs'">Fav tasks</button>
 		</nav>
-		<div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+		<div class="loading" v-if="loading">Loading tasks...</div>
 		<div v-if="filter === 'all'" class="task-list">
-			<p v-if="!taskStore.loading">
-				You have {{ taskStore.totalCount }} tasks left to do.
-			</p>
-			<div v-for="task in taskStore.tasks" :key="task.id">
+			<p v-if="!loading">You have {{ totalCount }} tasks left to do.</p>
+			<div v-for="task in tasks" :key="task.id">
 				<TaskDetail :task="task" />
 			</div>
 		</div>
 		<div v-if="filter === 'favs'" class="task-list">
-			<p v-if="!taskStore.loading">
-				You have {{ taskStore.favCount }} tasks in your favs list.
-			</p>
-			<div v-for="task in taskStore.favs" :key="task.id">
+			<p v-if="!loading">You have {{ favCount }} tasks in your favs list.</p>
+			<div v-for="task in favs" :key="task.id">
 				<TaskDetail :task="task" />
 			</div>
 		</div>
@@ -37,7 +33,20 @@
 	import TaskDetail from "./components/TaskDetail.vue";
 	import TaskForm from "./components/TaskForm.vue";
 	import { ref } from "vue";
+	import { storeToRefs } from "pinia";
 	const taskStore = useTaskStore();
+
+	const {
+		tasks,
+		loading,
+		getTasks,
+		addTask,
+		deleteTask,
+		toggleFav,
+		favs,
+		favCount,
+		totalCount,
+	} = storeToRefs(taskStore);
 	const filter = ref("all");
 	taskStore.getTasks();
 </script>
